a8a30fde44016cdf2c5086b1215221c7182f267f
zibas <milo.brontesi@mail.polimi.it>
2022-04-15 17:02:20 +0200

    test sulla connessione al database

--- src/main/java/it/polimi/tiw/jdbctest/ConnectionTester.java
+++ src/main/java/it/polimi/tiw/jdbctest/ConnectionTester.java
@@ -1,41 +1,33 @@
 package it.polimi.tiw.jdbctest;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ConnectionTester
- */
-@WebServlet("/ConnectionTester")
 public class ConnectionTester extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ConnectionTester() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		final String DB_URL = "jdbc:mysql://localhost:3306/progettoTIW?serverTimezone=UTC";
+		final String USER = "root";
+		final String PASS = "milone1";
+		String result = "Connection worked";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			DriverManager.getConnection(DB_URL, USER, PASS);
+		} catch (Exception e) {
+			result = "Connection failed";
+			e.printStackTrace();
+		}
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		out.println(result);
+		out.close();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
--- src/main/java/it/polimi/tiw/test/TestServlet.java
+++ src/main/java/it/polimi/tiw/test/TestServlet.java
@@ -1,40 +1,17 @@
 package it.polimi.tiw.test;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-/**
- * Servlet implementation class TestServlet
- */
-@WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public TestServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		out.println("Hello this is a test");
+		out.close();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
