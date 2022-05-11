59b4cdd85c7b2bde253d0caee3a5e90c42bc09f5
zibas <milo.brontesi@gmail.com>
2022-05-11 19:29:14 +0200

    bozza album page

--- src/main/java/it/polimi/tiw/controllers/GoToAlbumPage.java
+++ src/main/java/it/polimi/tiw/controllers/GoToAlbumPage.java
@@ -53,29 +53,41 @@ public class GoToAlbumPage extends HttpServlet{
 		this.templateEngine.setTemplateResolver(templateResolver);
 		templateResolver.setSuffix(".html");
 	}
-
+	
+	private List<Image> sliceImages(List<Image> images, int page){
+		int from = page * 5 - 5;
+		int to = images.size() > page * 5 - 1 ? page * 5 : images.size();
+		
+		return images.subList(from, to);
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {	
 		List<Image> images = null;
+		int neededPages = 1;
 		
 		String albumIDStr = request.getParameter("id");
+		String pageStr = request.getParameter("page");
 		
-		if (albumIDStr == null || albumIDStr.isEmpty()) {
+		if (albumIDStr == null || albumIDStr.isEmpty() || pageStr == null || pageStr.isEmpty()) {
 			response.sendError(505, "Parameters incomplete");
 			return;
 		}
 		
 		ImageDAO bService = new ImageDAO(connection);
 		int albumID = 0;
+		int currPage = 0;
 		String error = null;
 		try {
 			albumID = Integer.parseInt(albumIDStr);
+			currPage = Integer.parseInt(pageStr);
 		} catch (NumberFormatException e2) {
 			error = "Bad post ID input";
 		}
 		
 		try {
 			images = bService.findImagesFromAlbum(albumID);
+			neededPages = (int)Math.ceil(((double)images.size()) / 5);
 		} catch (Exception e) {
 			e.printStackTrace();
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
@@ -89,7 +101,10 @@ public class GoToAlbumPage extends HttpServlet{
 			String path = "/WEB-INF/album.html";
 			ServletContext servletContext = getServletContext();
 			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-			ctx.setVariable("images", images);
+			ctx.setVariable("images", sliceImages(images, currPage));
+			ctx.setVariable("pages", neededPages);
+			ctx.setVariable("nextPage", currPage + 1);
+			ctx.setVariable("prevPage", currPage - 1);
 			templateEngine.process(path, ctx, response.getWriter());
 		}
 		
@@ -99,6 +114,8 @@ public class GoToAlbumPage extends HttpServlet{
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
+	
+	
 	
 	public void destroy() {
 		try {
