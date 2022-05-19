4353a7f3991a750e49254a276437a8ce8e9449af
Andrea Colombo <andreaclashth9@gmail.com>
2022-05-19 12:22:19 +0200

    created Logout servlet

---
+++ src/main/java/it/polimi/tiw/controllers/Logout.java
@@ -0,0 +1,32 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Logout extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		//metodo alternativo -> request.getRequestDispatcher("").include(request, response);
+		
+		response.sendRedirect(getServletContext().getContextPath()+"/index.html");
+
+		HttpSession userSession = request.getSession();
+		userSession.invalidate();
+
+		out.print("You are successfully logged out!");
+
+		out.close();
+	}
+}
