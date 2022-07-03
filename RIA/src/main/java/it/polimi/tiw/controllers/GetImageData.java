220e75baf3a04f2865d9d39649c08cf15e5169b7
Andrea Colombo <andreaclashth9@gmail.com>
2022-07-03 17:07:01 +0200

    fixed GetAlbumData and added GetImageData

--- RIA/src/main/java/it/polimi/tiw/controllers/GetImageData.java
+++ RIA/src/main/java/it/polimi/tiw/controllers/GetImageData.java
@@ -15,13 +15,16 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import it.polimi.tiw.beans.Comment;
 import it.polimi.tiw.beans.Image;
 //import it.polimi.tiw.beans.User;
 import it.polimi.tiw.controllers.utils.ConnectionHandler;
+import it.polimi.tiw.dao.CommentDAO;
 import it.polimi.tiw.dao.ImageDAO;
 
-@WebServlet("/GetAlbumData")
-public class GetAlbumData extends HttpServlet {
+
+@WebServlet("/GetImageData")
+public class GetImageData extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
 
@@ -32,41 +35,51 @@ public class GetAlbumData extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		Integer albumId = null;
+		Integer imageId = null;
 		try {
-			albumId = Integer.parseInt(req.getParameter("albumid"));
+			imageId = Integer.parseInt(req.getParameter("imageId"));
 		} catch (NumberFormatException | NullPointerException e) {
 			// only for debugging e.printStackTrace();
 			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			resp.getWriter().println("Incorrect param values");
 			return;
 		}
-
-		//HttpSession session = req.getSession();
-		//User user = (User) session.getAttribute("user");
-
-		List<Image> albumImages = null;
+		
+		Image albumImage = null;
 
 		ImageDAO imageService = new ImageDAO(connection);
 
 		try {
-			albumImages = imageService.findImagesFromAlbum(albumId);
+			albumImage = imageService.findImage(imageId);
 		} catch (SQLException e) {
 			resp.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
 			e.printStackTrace();
-			resp.getWriter().println("Error in retrieving images from databse");
+			resp.getWriter().println("Error in retrieving image from databse");
+			return;
+		}
+	
+		List<Comment> imageComments = null;
+		CommentDAO commentService = new CommentDAO(connection);
+		
+		try {
+			imageComments = commentService.findCommentsForImage(imageId);
+		} catch (SQLException e) {
+			resp.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
+			e.printStackTrace();
+			resp.getWriter().println("Error in retrieving comments from databse");
 			return;
 		}
 
 		Gson gson = new GsonBuilder().setDateFormat("yyyy MMM dd").create();
-		String albumImagesJson = gson.toJson(albumImages);
+		String albumImageJson = gson.toJson(albumImage);
+		String imagesCommentJson = gson.toJson(imageComments);
+		String bothJson = "[" + albumImageJson + "," + imagesCommentJson + "]";
 		resp.setStatus(HttpServletResponse.SC_OK);
 		resp.setContentType("application/json");
 		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().write(albumImagesJson);
-
+		resp.getWriter().write(bothJson);
+		
 	}
-
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
@@ -82,3 +95,4 @@ public class GetAlbumData extends HttpServlet {
 	}
 
 }
+
